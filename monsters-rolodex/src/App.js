e7c6b86e788b4d32127d8a7882db7435ee3151fa
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-11 22:11:32 +0200

    Section 3: React Basics | Monsters Rolodex - Component State

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,22 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Yuliia',
+    };
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name}!</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
