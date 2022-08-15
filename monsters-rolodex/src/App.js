bae89f5bbd70fd48eb947c68466513b6cc6a1c86
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-15 20:31:54 +0200

    Section 3: React Basics | Monsters Rolodex - Hooks: useState

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import CardList from './components/card-list/cardList.component';
 import SearchBox from './components/search-box/searchBox.component';
@@ -7,86 +7,106 @@ import './App.css';
 const API_CHAR_NAMES = 'https://api.genshin.dev/characters';
 const API_CHAR_OBJS = 'https://api.genshin.dev/characters/all';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState(''); // [value, setValue]
 
-    this.state = {
-      charList: [],
-      charNames: [],
-      searchField: '',
-    };
-  }
-
-  componentDidMount() {
-    this.getCharacters();
-    this.getCharNames();
-  }
-
-  getCharacters() {
-    fetch(API_CHAR_OBJS)
-      .then((response) => response.json())
-      .then((chars) => {
-        const filteredCharObjs = chars.filter(
-          (char) =>
-            !char.name.toLowerCase().includes('thoma') &&
-            !char.name.toLowerCase().includes('traveler')
-        );
-
-        this.setState(() => {
-          return { charList: filteredCharObjs };
-        });
-      });
-  }
-
-  getCharNames() {
-    fetch(API_CHAR_NAMES)
-      .then((response) => response.json())
-      .then((charNames) => {
-        const filteredCharNames = charNames.filter(
-          (name) => !name.includes('thoma') && !name.includes('traveler')
-        );
-
-        this.setState(() => {
-          return { charNames: filteredCharNames };
-        });
-      });
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { onSearchChange } = this;
-    const { charList, charNames, searchField } = this.state;
+  return (
+    <div className='App'>
+      <SearchBox
+        className='chars-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='Search characters'
+      />
+      {/* <CardList chars={filteredChars} charNames={filteredCharNames} /> */}
+    </div>
+  );
+};
 
-    const filteredChars = charList.filter((char) =>
-      char.name.toLowerCase().startsWith(searchField)
-    );
+// class AppClass extends Component {
+//   constructor() {
+//     super();
 
-    const filteredCharNames = charNames.filter((charName) =>
-      charName.startsWith(searchField)
-    );
+//     this.state = {
+//       charList: [],
+//       charNames: [],
+//       searchField: '',
+//     };
+//   }
 
-    console.log(filteredChars);
-    console.log(filteredCharNames);
+//   componentDidMount() {
+//     this.getCharacters();
+//     this.getCharNames();
+//   }
 
-    return (
-      <div className='App'>
-        <SearchBox
-          className='chars-search-box'
-          onChangeHandler={onSearchChange}
-          placeholder='Search characters'
-        />
-        <CardList chars={filteredChars} charNames={filteredCharNames} />
-      </div>
-    );
-  }
-}
+//   getCharacters() {
+//     fetch(API_CHAR_OBJS)
+//       .then((response) => response.json())
+//       .then((chars) => {
+//         const filteredCharObjs = chars.filter(
+//           (char) =>
+//             !char.name.toLowerCase().includes('thoma') &&
+//             !char.name.toLowerCase().includes('traveler')
+//         );
+
+//         this.setState(() => {
+//           return { charList: filteredCharObjs };
+//         });
+//       });
+//   }
+
+//   getCharNames() {
+//     fetch(API_CHAR_NAMES)
+//       .then((response) => response.json())
+//       .then((charNames) => {
+//         const filteredCharNames = charNames.filter(
+//           (name) => !name.includes('thoma') && !name.includes('traveler')
+//         );
+
+//         this.setState(() => {
+//           return { charNames: filteredCharNames };
+//         });
+//       });
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLowerCase();
+
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+
+//   render() {
+//     const { onSearchChange } = this;
+//     const { charList, charNames, searchField } = this.state;
+
+//     const filteredChars = charList.filter((char) =>
+//       char.name.toLowerCase().startsWith(searchField)
+//     );
+
+//     const filteredCharNames = charNames.filter((charName) =>
+//       charName.startsWith(searchField)
+//     );
+
+//     console.log(filteredChars);
+//     console.log(filteredCharNames);
+
+//     return (
+//       <div className='App'>
+//         <SearchBox
+//           className='chars-search-box'
+//           onChangeHandler={onSearchChange}
+//           placeholder='Search characters'
+//         />
+//         <CardList chars={filteredChars} charNames={filteredCharNames} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
