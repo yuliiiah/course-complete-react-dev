88cfb60850b6ffaa98e32779d82b1f9d2497da18
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-16 22:21:55 +0200

    Section 3: React Basics | React and ReactDOM Pt.2

---
+++ pure-react/app.js
@@ -0,0 +1,26 @@
+const Person = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.occupation),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', { className: 'title' }, 'React IS rendered'),
+    React.createElement(Person, { name: 'Yuliia', occupation: 'idiot' }, null),
+    React.createElement(
+      Person,
+      { name: 'Yihua', occupation: 'instructor' },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: 'Andrei', occupation: 'insctructor' },
+      null
+    ),
+    React.createElement(Person, { name: 'Yosia', occupation: 'cat' }, null),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
