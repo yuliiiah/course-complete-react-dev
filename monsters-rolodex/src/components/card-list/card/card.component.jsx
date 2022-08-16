255be5f42ffa7694fdc9722da4dd2b76226bede7
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-16 21:46:50 +0200

    Section 3: React Basics | Monsters Rolodex - Remaining Components

--- monsters-rolodex/src/components/card-list/card/card.component.jsx
+++ monsters-rolodex/src/components/card-list/card/card.component.jsx
@@ -1,26 +1,20 @@
-import { Component } from 'react';
-
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const { char, idx, charNames } = this.props;
+const Card = ({ char, idx, charNames }) => {
+  const { name, description } = char;
 
-    const { name, description } = char;
+  return (
+    <div key={idx} className='card-container'>
+      <img
+        className='char-icon'
+        src={`https://api.genshin.dev/characters/${charNames[idx]}/icon-big`}
+        alt={name}
+      />
 
-    return (
-      <div key={idx} className='card-container'>
-        <img
-          className='char-icon'
-          src={`https://api.genshin.dev/characters/${charNames[idx]}/icon-big`}
-          alt={name}
-        />
-
-        <h2 className='char-name'>{name}</h2>
-        <p className='char-descr'>{description}</p>
-      </div>
-    );
-  }
-}
+      <h2 className='char-name'>{name}</h2>
+      <p className='char-descr'>{description}</p>
+    </div>
+  );
+};
 
 export default Card;
--- monsters-rolodex/src/components/card-list/cardList.component.jsx
+++ monsters-rolodex/src/components/card-list/cardList.component.jsx
@@ -1,20 +1,15 @@
-import { Component } from 'react';
 import Card from './card/card.component';
 
 import './cardList.styles.css';
 
-class CardList extends Component {
-  render() {
-    const { chars, charNames } = this.props;
-
-    return (
-      <div className='card-list'>
-        {chars.map((char, idx) => {
-          return <Card idx={idx} char={char} charNames={charNames} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ chars, charNames }) => {
+  return (
+    <div className='card-list'>
+      {chars.map((char, idx) => {
+        return <Card idx={idx} char={char} charNames={charNames} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
