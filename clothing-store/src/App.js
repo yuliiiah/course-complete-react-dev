3e8a15cd2e10d5ea5f8da21fba9d9a35adf84902
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-17 21:48:37 +0200

    Section 4: Capstone Project | Adding Sass

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss';
+
 const App = () => {
   const categories = [
     {
@@ -24,9 +26,9 @@ const App = () => {
 
   return (
     <div className='categories-container'>
-      {categories.map(({ title }) => {
+      {categories.map(({ id, title }) => {
         return (
-          <div className='category-container'>
+          <div key={id} className='category-container'>
             <div className='background-image' />
             <div className='category-body-container'>
               <h2>{title}</h2>
