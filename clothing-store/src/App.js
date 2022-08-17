7120a3c9ab7a4f7a67d9d1ce5fb9255d41668907
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-17 22:02:26 +0200

    Section 4: Capstone Project | Category Item + Directory Components

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,44 +1,37 @@
+import CategoryMenu from './components/category-menu/category-menu.comp';
+
 import './categories.styles.scss';
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
-      title: 'Jackets',
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
-      title: 'Women',
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
-      title: 'Men',
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
-  return (
-    <div className='categories-container'>
-      {categories.map(({ id, title }) => {
-        return (
-          <div key={id} className='category-container'>
-            <div className='background-image' />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <CategoryMenu categories={categories} />;
 };
 
 export default App;
