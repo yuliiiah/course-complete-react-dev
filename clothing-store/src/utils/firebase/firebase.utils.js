04b42941ce8a108e8d21838ceee1408f82eaa2f9
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-20 21:15:52 +0200

    Section 6: Auth + Firebase | Setting Up User Docs

--- clothing-store/src/utils/firebase/firebase.utils.js
+++ clothing-store/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBhtNAi91FVKL-VQLBX-VhAjlRePu3DRt4',
@@ -26,3 +28,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
