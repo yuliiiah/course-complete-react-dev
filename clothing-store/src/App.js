e0fce24d6b088251d2f4b4ad2d7089055ae0889a
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-20 20:21:12 +0200

    Section 6: Auth + Firebase | Setting Up Firebase

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.comp';
 import Navigation from './routes/nav/nav.comp';
+import SignIn from './routes/sign-in/signIn.comp';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
