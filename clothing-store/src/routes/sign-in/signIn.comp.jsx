4534cec8eba34934ba82ffc1c541521c8940d349
yuliiiah <horbenko.yuliya@gmail.com>
2022-08-20 21:37:10 +0200

    Section 6: Auth + Firebase | Sign in with redirect

--- clothing-store/src/routes/sign-in/signIn.comp.jsx
+++ clothing-store/src/routes/sign-in/signIn.comp.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    (async function getGoogleRedirectRes() {
+      const response = await getRedirectResult(auth);
+
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    })();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
