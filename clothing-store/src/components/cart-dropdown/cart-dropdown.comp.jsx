00d23a87d7bfc79ea43951f83ed8b0b863d78e1e
yuliiiah <horbenko.yuliya@gmail.com>
2022-09-01 22:56:28 +0200

    Section 9: React Context Continued | Add To Cart

--- clothing-store/src/components/cart-dropdown/cart-dropdown.comp.jsx
+++ clothing-store/src/components/cart-dropdown/cart-dropdown.comp.jsx
@@ -1,11 +1,22 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.comp';
+import CartItem from '../cart-item/cart-item.comp';
 
 import './cart-dropdown.style.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>Checkout</Button>
     </div>
   );
