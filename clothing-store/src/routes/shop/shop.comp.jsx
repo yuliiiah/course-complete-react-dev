a4cad4c5f4646a51b2acb2d456973720cdd87b9a
yuliiiah <horbenko.yuliya@gmail.com>
2022-09-07 19:27:02 +0200

    Section 10: Firebase Database Storage | Nested routes in Shop

--- clothing-store/src/routes/shop/shop.comp.jsx
+++ clothing-store/src/routes/shop/shop.comp.jsx
@@ -1,21 +1,14 @@
-import { useContext, Fragment } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.comp';
+import { Routes, Route } from 'react-router-dom';
+
+import CategoriesPreview from '../categories-preview/categories-preview.comp';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
