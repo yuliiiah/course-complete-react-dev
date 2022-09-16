0805c381d2b82e4a2fce0cf733cf7ae022628e36
yuliiiah <horbenko.yuliya@gmail.com>
2022-09-16 23:55:42 +0200

    Section 33: Build a GatsbyJS Blog | Gatsby GraphQL + Markdown

--- gatsby-blog/gatsby-config.js
+++ gatsby-blog/gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `mds`,
+        path: `${__dirname}/src/md-pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
